0cc43236c03f275ddc22a55b42044c7fb28670ca
oooyuanooo <96405319+oooyuanooo@users.noreply.github.com>
2022-12-30 12:29:37 +0800

    Update Maj_logic.v

--- Maj_logic.v
+++ Maj_logic.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 27.06.2022 23:45:05
-// Design Name: 
-// Module Name: Maj_logic
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module Maj_logic(
